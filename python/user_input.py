2067155b7e620edad86a1fbebb470079ebbfaecf
fredkd <kimanifredrick807@gmail.com>
2024-03-25 04:58:16 -0400

    added a python file

---
+++ python/user_input.py
@@ -0,0 +1,10 @@
+# ask user for their name, age and location.
+
+
+name = input("What is your name ? ")
+age = input("What is your age ? ")
+location = input("Where are you located ? ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+
